ba2033736fe629fc70e48a806a61b57184af6cf4
sikehish <hisham0502@gmail.com>
2024-02-13 17:18:03 +0530

    Basic get request event handler created

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default() // Create a new Gin router
+	server.GET("/events", getEvents)
+	server.Run(":8080")
+}
+
+func getEvents(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{ //http.StatusOK is the same as Status code 200. You can use it interchangably
+		"message": "Hello!",
+	})
 }
