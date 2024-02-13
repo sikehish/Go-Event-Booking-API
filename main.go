3dc185f5ccce588eb7156d37add906064a12efae
sikehish <hisham0502@gmail.com>
2024-02-13 19:50:58 +0530

    Created a post request handler

--- main.go
+++ main.go
@@ -4,16 +4,36 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/sikehish/Go-Event-Booking-API/models"
 )
 
 func main() {
 	server := gin.Default()          // Create a new Gin router
 	server.GET("/events", getEvents) //Or you can have an anonymous function in place ofgetEvents
-	server.Run(":8080")              //localhost:8080
+	server.POST("/events", createEvent)
+	server.Run(":8080") //localhost:8080
 }
 
 func getEvents(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{ //http.StatusOK is the same as Status code 200. You can use it interchangably
-		"message": "Hello!",
-	})
+	// context.JSON(http.StatusOK, gin.H{ //http.StatusOK is the same as Status code 200. You can use it interchangably
+	// 	"message": "Hello!",
+	// })
+
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event) //event is populated with data. It doesnt return any error if any field is missing, but sing struct tags we can enforce ShouldBindJSON to return an error if a field is missing
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data"})
+	}
+
+	event.ID = 1
+	event.UserID = 1
+
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created", "event": event})
+
 }
