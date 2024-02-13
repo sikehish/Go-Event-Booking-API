78800e7095efb44d2ed7483f001d8c413a50e06b
sikehish <hisham0502@gmail.com>
2024-02-13 20:00:42 +0530

    Modified the post request handler

--- main.go
+++ main.go
@@ -34,6 +34,8 @@ func createEvent(context *gin.Context) {
 	event.ID = 1
 	event.UserID = 1
 
+	event.Save()
+
 	context.JSON(http.StatusCreated, gin.H{"message": "Event created", "event": event})
 
 }
