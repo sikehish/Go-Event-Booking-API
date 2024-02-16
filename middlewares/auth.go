5559c9792e79d57def834c9eac3fe311d129de7f
sikehish <hisham0502@gmail.com>
2024-02-16 10:08:57 +0530

    Created an auth middleware and middlewares package

---
+++ middlewares/auth.go
@@ -0,0 +1,28 @@
+package middlewares
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sikehish/Go-Event-Booking-API/utils"
+)
+
+func Authenticate(context *gin.Context) {
+	token := context.Request.Header.Get("Authorization")
+
+	if token == "" {
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Not authorized"})
+		return
+		// AbortWithStatusJSON calls `Abort()` and then `JSON` internally. This method stops the chain, writes the status code and return a JSON body.
+
+		//We dont use context.JSON() as other request handlers in line will still be executed even after sending a response in in this middleware, and we want to ensure that the execution is completely stopped as the user isnt authorized, and that's why we use AbortWithStatusJSON
+	}
+
+	userId, err := utils.VerifyToken(token)
+
+	if err != nil {
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Not authorized"})
+	}
+
+	context.Next() //Ensures that the next request handler in line would be executed
+}
