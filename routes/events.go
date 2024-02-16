92079ddad84eeb019d2a826fbdea394a3e2c37a4
sikehish <hisham0502@gmail.com>
2024-02-16 12:01:23 +0530

    Restricting users from Editing and Deleting

--- routes/events.go
+++ routes/events.go
@@ -95,12 +95,19 @@ func updateEvent(context *gin.Context) {
 		return
 	}
 
-	_, err = models.GetEventByID(eventId)
+	userId := context.GetInt64("userId")
+	event, err := models.GetEventByID(eventId)
 
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{
 			"message": "Event doesnt exist",
 		})
+		return
+	}
+
+	if event.UserID != userId {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized to update the event"})
+		return
 	}
 
 	var updatedEvent models.Event
@@ -131,12 +138,19 @@ func deleteEvent(context *gin.Context) {
 		return
 	}
 
+	userId := context.GetInt64("userId")
+
 	deletedEvent, err := models.GetEventByID(eventId)
 
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{
 			"message": "Event doesnt exist",
 		})
+	}
+
+	if deletedEvent.UserID != userId {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized to delete the event"})
+		return
 	}
 
 	err = deletedEvent.Delete()
